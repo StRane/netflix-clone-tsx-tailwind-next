ae2eec9563281828b7dacb1ade6e06861e43e0f2
StRane <73998688+StRane@users.noreply.github.com>
2022-10-19 20:48:32 +0700

    bug

---
+++ components/Plans.tsx
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import Link from "next/link";
+import useAuth from "../hooks/useAuth";
+import { HiCheck } from "react-icons/hi";
+
+function Plans() {
+  const { logout } = useAuth();
+
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Netflix"
+            width={150}
+            height={90}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <button
+          className="text-lg font-medium hover:underline"
+          onClick={logout}
+        >
+          Sign Out
+        </button>
+      </header>
+
+      <main className="pt-28 max-w-5xl px-5 pb-12 transition-all md:px-10">
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+
+        <ul>
+          <li className="flex items-center gap-x-2 text-lg">
+            <HiCheck className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <HiCheck className="h-7 w-7 text-[#E50914]" /> Recommendations just
+            for you.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <HiCheck className="h-7 w-7 text-[#E50914]" /> Change or cancel your
+            plan anytime.
+          </li>
+        </ul>
+
+        <div className="mt-4 flex flex-col space-y-4">
+          <div className="flex w-full items-center justify-end self-end md:w-3/5">
+            
+              <div className="planBox">asda</div>
+              <div className="planBox">asda</div>
+              <div className="planBox">asdas</div>
+              <div className="planBox">asdas</div>
+            
+          </div>
+
+         {/*  //Table */}
+
+         <button>Subscribe</button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Plans;
---
+++ pages/index.tsx
@@ -0,0 +1,121 @@
+import Head from "next/head";
+
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Row from "../components/Row";
+import Modal from "../components/Modal";
+import Plans from "../components/Plans";
+
+import requests from "../utilities/request";
+import { modalState } from "../atoms/modalAtom";
+import { Movie } from "../typings";
+import useAuth from "../hooks/useAuth";
+import { useRecoilValue } from "recoil";
+
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
+import payments from "../lib/stripe";
+
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+  products: Product[];
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+  products,
+}: Props) => {
+  const { loading } = useAuth();
+  const showModal = useRecoilValue(modalState);
+
+  console.log(products)
+
+  const subscription = false;
+
+  if (loading || subscription === null) return null;
+
+  if (!subscription) return <Plans />;
+
+  if (loading) return "loading";
+
+  return (
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+      <Head>
+        <title>Netflix Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="relative pl-4 pb-24 lg:space-y-24">
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List component*/}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
+      </main>
+      {showModal && <Modal />}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  }).then((res)=>res).catch((error)=> console.log(error));
+
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      products,
+    },
+  };
+};
